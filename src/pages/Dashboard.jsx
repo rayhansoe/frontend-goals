978e6bd3d5bc33a85a3eeb9708e1ee01a9e1ceb4
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-03-16 23:23:30 +0700

    olah data di Dashboard.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,31 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { reset } from '../features/users/userSlice'
 
 const Dashboard = () => {
 	const { user } = useSelector(state => state.auth)
 
 	const navigate = useNavigate()
+	const dispatch = useDispatch()
 
 	useEffect(() => {
 		if (!user) {
 			navigate('/login')
 		}
-	}, [user, navigate])
 
-	return <div>Dashboard</div>
+		dispatch(reset())
+	}, [user, navigate, dispatch])
+
+	return (
+		<>
+			<p>Dashboard</p>
+			<section className='heading'>
+				<h1>
+					Welcome <Link to={`/${user && user.username}`}>{user && user.name}</Link>
+				</h1>
+			</section>
+		</>
+	)
 }
 export default Dashboard
