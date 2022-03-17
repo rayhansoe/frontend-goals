7d024cde4d8d18835b31f598a7bebc76de5dea64
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-03-18 01:13:10 +0700

    if forgot to create update profile user API, so i just render the ui and removed the logic.

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,41 +1,10 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { toast } from 'react-toastify'
-
 const EditProfile = ({ userProfile }) => {
-	const [formData, setFormData] = useState({
-		username: '',
-		name: '',
-		email: '',
-	})
-
-	const { name, email, password, password2, username } = formData
-
-	const dispatch = useDispatch()
-
 	// form control
-	const onChange = e => {
-		setFormData(prevState => ({
-			...prevState,
-			[e.target.name]: e.target.value,
-		}))
-	}
+	const onChange = e => {}
 
 	// submit form.
 	const onSubmit = e => {
 		e.preventDefault()
-
-		if (password !== password2) {
-			toast.error('password do not match!')
-		} else {
-			const userData = {
-				username,
-				name,
-				email,
-			}
-
-			dispatch()
-		}
 	}
 
 	return (
@@ -44,36 +13,36 @@ const EditProfile = ({ userProfile }) => {
 				<form onSubmit={onSubmit}>
 					<div className='form-group'>
 						<input
-							required
+							disabled
 							className='form-control'
 							type='text'
 							id='username'
 							name='username'
-							value={username}
+							value={userProfile.username}
 							placeholder={userProfile.username}
 							onChange={onChange}
 						/>
 					</div>
 					<div className='form-group'>
 						<input
-							required
+							disabled
 							className='form-control'
 							type='text'
 							id='name'
 							name='name'
-							value={name}
+							value={userProfile.name}
 							placeholder={userProfile.name}
 							onChange={onChange}
 						/>
 					</div>
 					<div className='form-group'>
 						<input
-							required
+							disabled
 							className='form-control'
 							type='email'
 							id='email'
 							name='email'
-							value={email}
+							value={userProfile.email}
 							placeholder={userProfile.email}
 							onChange={onChange}
 						/>
