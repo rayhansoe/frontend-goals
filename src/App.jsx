bbc684beb2f9307685011f3084d43153cf7f0a85
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-03-18 01:11:36 +0700

    added route goal page.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
+import Goal from './pages/Goal'
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 						<Route path='/login' element={<Login />} />
 						<Route path='/register' element={<Register />} />
 						<Route path='/:username' element={<Profile />} />
+						<Route path='/goals/:goalId' element={<Goal />} />
 					</Routes>
 				</div>
 			</Router>
