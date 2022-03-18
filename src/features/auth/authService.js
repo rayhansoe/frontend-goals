36cf9133952e16551944c29c312bb16f9acb0b49
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-03-19 03:41:10 +0700

    fix set localStorage token.

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -21,7 +21,7 @@ const login = async userData => {
 
 	if (response.data) {
 		const { token, userProfile } = response.data
-		localStorage.setItem('token', JSON.stringify({ token }))
+		localStorage.setItem('token', JSON.stringify(token))
 		localStorage.setItem('user', JSON.stringify({ userProfile }))
 	}
 
